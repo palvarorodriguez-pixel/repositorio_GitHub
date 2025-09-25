5563e0732d20e8d6e85d498fbb46824ecee5dc79
palvarorodriguez-pixel <palvaro.rodriguez@aefcm.gob.mx>
2025-09-25 13:01:18 -0600

    Add files via upload

---
+++ repositorio_GitHub.bat
@@ -0,0 +1,7 @@
+# En tu carpeta del proyecto
+git init
+git add .
+git commit -m "Sistema de Vales de Resguardo DGA"
+git branch -M main
+git remote add origin https://github.com/tuusuario/sistema-vales-dga.git
+git push -u origin main
